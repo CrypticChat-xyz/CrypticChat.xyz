c8b692ec19ec0e421f49bcddba315627823a12f8
Cryptic Chat <cryptic.chat@outlook.com>
2024-03-16 11:50:15 -0500

    Update 1.0.1!

--- index.js
+++ index.js
@@ -2,14 +2,47 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const axios = require('axios');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const filter = new Filter();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', (socket) => {
+function containsSwearWords(message) {
+    return filter.isProfane(message);
+}
+
+function filterSwearWords(message) {
+    return filter.clean(message);
+}
+
+async function getCryptoPrices() {
+    try {
+        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple&vs_currencies=usd');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching cryptocurrency prices:', error);
+        return null;
+    }
+}
+
+io.on('connection', async (socket) => {
+    const prices = await getCryptoPrices();
+    if (prices) {
+        io.to(socket.id).emit('crypto prices', prices);
+    }
+
+    setInterval(async () => {
+        const updatedPrices = await getCryptoPrices();
+        if (updatedPrices) {
+            io.emit('crypto prices', updatedPrices);
+        }
+    }, 30000);
+
     socket.on('new user', (username) => {
         socket.username = username;
         io.emit('chat message', `${username} has joined the chat`);
@@ -23,16 +56,24 @@ io.on('connection', (socket) => {
 
     socket.on('chat message', (msg) => {
         if (msg.trim() !== '') {
-            console.log(`[${socket.handshake.address}] ${socket.username}: ${msg}`);
-            socket.emit('chat message', `${socket.username}: ${msg}`);
-            socket.broadcast.emit('chat message', `${socket.username}: ${msg}`);
+            const timestamp = new Date().toLocaleTimeString();
+            const messageWithTimestamp = `[${timestamp}] ${socket.username}: ${msg}`;
+            
+            if (containsSwearWords(msg)) {
+                const filteredMessage = filterSwearWords(msg);
+                io.emit('chat message', `[Filtered] ${socket.username}: ${filteredMessage}`);
+            } else {
+                io.emit('chat message', messageWithTimestamp);
+            }
+            
+            console.log(`[${socket.handshake.address}] ${messageWithTimestamp}`);
         }
     });
 });
 
-    app.use((req, res, next) => {
-        res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 server.listen(3000, () => {
     console.log(`
